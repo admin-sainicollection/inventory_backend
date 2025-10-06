f63d2858d98d99ca6365d661aa5d5d0f4ce637c2
Mukesh Kumar <mukesh.scroffice@gmail.com>
2025-10-06 15:52:54 +0530

    add category and upadte and delete

--- src/modules/inventory/category/category.controller.ts
+++ src/modules/inventory/category/category.controller.ts
@@ -15,3 +15,39 @@ export const addCategory = async (req: Request, res: Response) => {
         res.status(500).json({ message: error.message })
     }
 }
+
+export const updateCategory = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const { name, description } = req.body;
+
+        const category = await Category.findByIdAndUpdate(id, { name, description }, { new: true, runValidators: true });
+
+        if (!category) return res.status(404).json({ message: "Category not found" });
+        res.status(200).json({ message: "Category updated successfully", category });
+    } catch (error: any) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+export const deleteCategory = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.body;
+
+        const category = await Category.findByIdAndDelete(id);
+        if (!category) return res.status(404).json({ message: "Category not found" });
+        res.status(200).json({ message: "Category deleted successfully" });
+
+    } catch (error: any) {
+        res.status(500).json({ message: (error).message })
+    }
+}
+
+export const getAllCategories = async (req: Request, res: Response) => {
+    try {
+        const categories = await Category.find().sort({ createdAt: -1 });
+        res.status(200).json({ categories });
+    } catch (error: any) {
+        res.status(500).json({ message: (error).message })
+    }
+}
--- src/modules/inventory/category/category.validation.ts
+++ src/modules/inventory/category/category.validation.ts
@@ -4,3 +4,12 @@ export const createCategorySchema = z.object({
     name: z.string().min(2, "Category name must be atleast 2 charecters long"),
     description: z.string().optional()
 })
+
+export const updateCategorySchema = z.object({
+    name: z.string().min(2, "Category name must be atleast 2 characters long").optional(),
+    description: z.string().optional()
+})
+
+export const deleteCategorySchema = z.object({
+    id: z.string().min(1, "Category ID is required")
+})
