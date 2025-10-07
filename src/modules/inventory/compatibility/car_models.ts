4c12363febffc86a127b52fd449af2b1fe1d0159
Mukesh Kumar <mukesh.scroffice@gmail.com>
2025-10-07 10:27:54 +0530

    car models mock data and schema

---
+++ src/modules/inventory/compatibility/car_models.ts
@@ -0,0 +1,113 @@
+export const car_models = [
+    // Maruti Suzuki
+    "Maruti Suzuki Alto",
+    "Maruti Suzuki Alto K10",
+    "Maruti Suzuki Wagon R",
+    "Maruti Suzuki Celerio",
+    "Maruti Suzuki Swift",
+    "Maruti Suzuki Dzire",
+    "Maruti Suzuki Baleno",
+    "Maruti Suzuki Ignis",
+    "Maruti Suzuki Fronx",
+    "Maruti Suzuki Brezza",
+    "Maruti Suzuki Ertiga",
+    "Maruti Suzuki XL6",
+    "Maruti Suzuki Grand Vitara",
+    "Maruti Suzuki Eeco",
+
+    // Hyundai
+    "Hyundai Santro",
+    "Hyundai Grand i10 Nios",
+    "Hyundai i20",
+    "Hyundai Aura",
+    "Hyundai Exter",
+    "Hyundai Venue",
+    "Hyundai Creta",
+    "Hyundai Alcazar",
+    "Hyundai Verna",
+    "Hyundai Tucson",
+
+    // Tata Motors
+    "Tata Tiago",
+    "Tata Tigor",
+    "Tata Altroz",
+    "Tata Punch",
+    "Tata Nexon",
+    "Tata Harrier",
+    "Tata Safari",
+
+    // Mahindra
+    "Mahindra Bolero",
+    "Mahindra Bolero Neo",
+    "Mahindra Scorpio",
+    "Mahindra Scorpio N",
+    "Mahindra Thar",
+    "Mahindra XUV300",
+    "Mahindra XUV400 EV",
+    "Mahindra XUV500",
+    "Mahindra XUV700",
+
+    // Kia
+    "Kia Sonet",
+    "Kia Seltos",
+    "Kia Carens",
+
+    // Toyota
+    "Toyota Glanza",
+    "Toyota Urban Cruiser Hyryder",
+    "Toyota Innova Crysta",
+    "Toyota Innova Hycross",
+    "Toyota Fortuner",
+    "Toyota Rumion",
+    "Toyota Hilux",
+
+    // Honda
+    "Honda Amaze",
+    "Honda City",
+    "Honda Elevate",
+    "Honda WR-V",
+
+    // Skoda
+    "Skoda Slavia",
+    "Skoda Kushaq",
+    "Skoda Rapid",
+    "Skoda Superb",
+
+    // Volkswagen
+    "Volkswagen Polo",
+    "Volkswagen Virtus",
+    "Volkswagen Taigun",
+    "Volkswagen Tiguan",
+
+    // MG
+    "MG Comet EV",
+    "MG Astor",
+    "MG Hector",
+    "MG Hector Plus",
+    "MG Gloster",
+
+    // Renault
+    "Renault Kwid",
+    "Renault Triber",
+    "Renault Kiger",
+
+    // Nissan
+    "Nissan Magnite",
+
+    // Citroen
+    "Citroen C3",
+    "Citroen eC3",
+    "Citroen C3 Aircross",
+
+    // Jeep
+    "Jeep Compass",
+    "Jeep Meridian",
+    "Jeep Wrangler",
+
+    // EVs & Compact Urban Cars
+    "Tata Tiago EV",
+    "Tata Tigor EV",
+    "Tata Nexon EV",
+    "MG ZS EV",
+    "Hyundai Kona Electric",
+]
