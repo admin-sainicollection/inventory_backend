e36ddf9c79b10c79d79edbe04ed299c8392e4af4
Mukesh Kumar <mukesh.scroffice@gmail.com>
2025-11-11 15:32:13 +0530

    alter controller and model of category for add aliasnames

--- src/modules/inventory/category/category.model.ts
+++ src/modules/inventory/category/category.model.ts
@@ -2,25 +2,31 @@ import mongoose, { Document, Schema } from "mongoose";
 
 export interface ICategory extends Document {
     name: string;
+    aliasNames?: string[];
     description?: string;
-    attributesTemplate?:{
-        key:string,
-        label:string,
-        type:string,
-        required:boolean,
-        options?:string[]
+    attributesTemplate?: {
+        key: string,
+        label: string,
+        type: string,
+        required: boolean,
+        options?: string[]
     }[];
 }
 
 const CategorySchema = new Schema<ICategory>({
     name: { type: String, required: true, unique: true, index: true },
+    aliasNames: {
+        type: [String],
+        trim: true,
+        default: [],
+    },
     description: { type: String, trim: true },
-    attributesTemplate:[{
-        key:{type:String, require:true},
-        label:{type:String, require:true},
-        type:{type:String, require:true},
-        required:{type:Boolean, require:true},
-        options:[String]
+    attributesTemplate: [{
+        key: { type: String, require: true },
+        label: { type: String, require: true },
+        type: { type: String, require: true },
+        required: { type: Boolean, require: true },
+        options: [String]
     }]
 }, {
     timestamps: true
