8cbcfb440d8bd354110c111422be1d4930c2f88e
Mukesh Kumar <mukesh.scroffice@gmail.com>
2025-12-19 12:43:25 +0530

    create schema nd validation for vendor daily ledger

---
+++ src/modules/vendorDailyLedger/vendorDailyLedger.validation.ts
@@ -0,0 +1,25 @@
+import z from 'zod';
+
+export const addVendorDailyLedgerValidation = z.object({
+    vendorId: z.string().trim().min(1, "Vendor ID is required"),
+    
+    date: z.string().optional(),
+    
+    voucher: z.string().trim().optional(),
+    
+    srNo: z.string().trim().optional(),
+    
+    credit: z.number().min(0, "Credit cannot be negative").optional(),
+    
+    debit: z.number().min(0, "Debit cannot be negative").optional(),
+
+    description: z.string().optional(),
+    
+    // tdsDeductByParty: z.number().min(0, "TDS deducted by party cannot be negative").optional(),
+    
+    // tdsDeductBySelf: z.number().min(0, "TDS deducted by self cannot be negative").optional(),
+    
+    status: z.enum(["active", "inactive"]).default("active"),
+});
+
+export const updateVendorDailyLedgerValidation = addVendorDailyLedgerValidation.partial();
