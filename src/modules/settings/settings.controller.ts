99b82fa1c49b91cad2c9a9fe4e29e1656968bc96
manojk.scr <manojk.scr@gmail.com>
2026-04-06 10:33:15 +0530

    settings.controllerremoved comments code

--- src/modules/settings/settings.controller.ts
+++ src/modules/settings/settings.controller.ts
@@ -4,17 +4,6 @@ import { deleteFile } from "../../utils/deleteFile";
 import Settings from "./settings.model";
 
 
-// export const saveSettings = async (req: Request, res: Response) => {
-//   try {
-//     const result = await createOrUpdateSettings(req.body);
-
-//     res.status(200).json(result);
-//   } catch (error: any) {
-//     res.status(500).json({ message: error.message });
-//   }
-// };
-
-
 export const fetchSettings = async (req: Request, res: Response) => {
   try {
     const result = await getSettings();
